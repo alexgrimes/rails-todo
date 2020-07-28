0f76a7435fbd065325b23c77c7b412276a981295
github username <github email address>
2020-07-28 12:37:01 -0500

    route updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :todos
+  get 'users/:id/notes', to: "user#notes"
+  get 'notes/:id', to: "notes#show"
+  delete 'notes/:id', to: "notes#destroy"
+  post 'notes/new', to: "notes#create"
+  patch 'notes/:id', to: "notes#update"
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
 end
